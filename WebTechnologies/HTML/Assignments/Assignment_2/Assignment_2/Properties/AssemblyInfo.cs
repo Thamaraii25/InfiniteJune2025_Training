1f9b4a848f76fafee893c329988c98fb5308cf9a
Thamaraipriya Madhaiyan <thamaraipriyam@infinite.com>
2025-08-03 18:14:01 +0530

    HTML Assignment

--- WebTechnologies/HTML/Assignments/Assignment_2/Assignment_2/Properties/AssemblyInfo.cs
+++ WebTechnologies/HTML/Assignments/Assignment_2/Assignment_2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SQLConnection")]
+[assembly: AssemblyTitle("Assignment_2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SQLConnection")]
+[assembly: AssemblyProduct("Assignment_2")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7318365d-0ded-43ac-aca6-4c7742f38384")]
+[assembly: Guid("86679b9e-530f-4983-b38e-6b0f80b14e59")]
 
 // Version information for an assembly consists of the following four values:
 //
